1789fc63696ecd552ff7bf01f51d70a8ffbb83e7
sontx <xuanson33bk@gmail.com>
2016-11-17 02:37:20 +0700

    * missing tag class

---
+++ ServerSide/Coffee360Service/src/com/dutproject/coffee360/model/bean/Tag.java
@@ -0,0 +1,25 @@
+package com.dutproject.coffee360.model.bean;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "tag")
+public class Tag {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
