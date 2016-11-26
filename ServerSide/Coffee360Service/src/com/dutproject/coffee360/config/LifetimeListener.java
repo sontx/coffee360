697d76dbfaf7ee818dd5dddbdcd00efbebb32222
minhthaobk94 <minhthaobk94@gmail.com>
2016-11-27 04:01:30 +0700

    - change images directory to outside

--- ServerSide/Coffee360Service/src/com/dutproject/coffee360/config/LifetimeListener.java
+++ ServerSide/Coffee360Service/src/com/dutproject/coffee360/config/LifetimeListener.java
@@ -15,7 +15,8 @@ public class LifetimeListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		String webappPath = event.getServletContext().getRealPath("");
 		System.getProperties().put(Constants.PROP_KEY_WEBAPP_PATH, webappPath);
-		System.getProperties().put(Constants.PROP_KEY_RESOURCE_DIR, new File(event.getServletContext().getRealPath("/"), "coffee360").getAbsolutePath());
+		//System.getProperties().put(Constants.PROP_KEY_RESOURCE_DIR, new File(event.getServletContext().getRealPath("/"), "coffee360").getAbsolutePath());
+		System.getProperties().put(Constants.PROP_KEY_RESOURCE_DIR, "/var/lib/tomcat8/webapps/coffee360");
 		System.getProperties().put(Constants.PROP_KEY_RESOURCE_PHOTO_DIR_NAME, "photos");
 	}
 }
