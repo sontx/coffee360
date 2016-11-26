cb713af0f373cab90b0e3c2435471181fd97a913
minhthaobk94 <minhthaobk94@gmail.com>
2016-11-26 23:10:06 +0700

    - save images to web app directory

--- ServerSide/Coffee360Service/src/com/dutproject/coffee360/config/LifetimeListener.java
+++ ServerSide/Coffee360Service/src/com/dutproject/coffee360/config/LifetimeListener.java
@@ -1,5 +1,6 @@
 package com.dutproject.coffee360.config;
 
+import java.io.File;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -14,8 +15,7 @@ public class LifetimeListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		String webappPath = event.getServletContext().getRealPath("");
 		System.getProperties().put(Constants.PROP_KEY_WEBAPP_PATH, webappPath);
-		System.getProperties().put(Constants.PROP_KEY_RESOURCE_DIR, "D:\\test");
+		System.getProperties().put(Constants.PROP_KEY_RESOURCE_DIR, new File(event.getServletContext().getRealPath("/"), "coffee360").getAbsolutePath());
 		System.getProperties().put(Constants.PROP_KEY_RESOURCE_PHOTO_DIR_NAME, "photos");
 	}
-
 }
